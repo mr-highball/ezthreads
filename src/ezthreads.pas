059c0300799cad4503090c5ed5ae3e7ddf3ae9c4
mr-highball <35942565+mr-highball@users.noreply.github.com>
2018-08-28 11:26:36 -0400

    further impl

--- src/ezthreads.pas
+++ src/ezthreads.pas
@@ -18,7 +18,7 @@ type
   TThreadCallback = procedure(Const AThread:IEZThread);
 
   (*
-    object method for exthreads
+    object method for ezthreads
   *)
   TThreadMethod = procedure(Const AThread:IEZThread) of object;
 
@@ -37,7 +37,7 @@ type
 
   { IEZThreadSettings }
   (*
-    settings for an ezthread
+    settings for an ezthread that deal with the operation of the thread
   *)
   IEZThreadSettings = interface
     ['{1EE5C618-DA56-4101-BE75-1FD25F131ED2}']
@@ -60,28 +60,69 @@ type
     function UpdateMaxRuntime(Const ARuntime:Cardinal):IEZThreadSettings;
   end;
 
+  { IEZThreadEvents }
+  (*
+    all events that can be registered with an ezthread
+  *)
+  IEZThreadEvents = interface
+    ['{7709EF05-BC0F-41A2-AB2B-2FCD8F305D8D}']
+    //property methods
+    function GetOnStart: TThreadMethod;
+    function GetOnStartCall: TThreadCallback;
+    function GetOnStop: TThreadMethod;
+    function GetOnStopCall: TThreadCallback;
+    function GetThread: IEZThread;
+
+    //properties
+    (*
+      called once the thread successfully starts
+    *)
+    property OnStart : TThreadMethod read GetOnStart;
+    property OnStartCallback : TThreadCallback read GetOnStartCall;
+
+    (*
+      called once the thread successfully stops either by force or by
+      finishing gracefully
+    *)
+    property OnStop : TThreadMethod read GetOnStop;
+    property OnStopCallback : TThreadCallback read GetOnStopCall;
+
+    (*
+      parent thread these settings belong to
+    *)
+    property Thread : IEZThread read GetThread;
+
+    //methods
+    function UpdateOnStart(Const AOnStart:TThreadMethod):IEZThreadEvents;
+    function UpdateOnStartCallback(Const AOnStart:TThreadCallback):IEZThreadEvents;
+    function UpdateOnStop(Const AOnStop:TThreadMethod):IEZThreadEvents;
+    function UpdateOnStopCallback(Const AOnStop:TThreadCallback):IEZThreadEvents;
+  end;
+
   (*
     IEZThread provides a simple to use and flexible interface
-    for performing tasks that require multithreading
+    for performing multithreaded tasks
   *)
   IEZThread = interface
     ['{998776BC-86C1-432D-B864-BC3A5FF6860A}']
     //property methods
     function GetSettings: IEZThreadSettings;
+    function GetEvents: IEZThreadEvents;
 
     //properties
     property Settings:IEZThreadSettings read GetSettings;
+    property Events:IEZThreadEvents read GetEvents;
 
     //methods
     (*
       add a single argument to this ezthread and an optional
-      cleanup method
+      cleanup method (will overwrite if name exists)
     *)
     function AddArg(Const AName:String;Const AArg:Variant;
-      OnFinish:TArgCleanupCallback=nil):IEZThread;overload;
+      OnFinish:TArgCleanupCallback):IEZThread;overload;
     function AddArg(Const AName:String;Const AArg:Variant;
-      OnFinish:TArgCleanupMethod=nil):IEZThread;overload;
-
+      OnFinish:TArgCleanupMethod):IEZThread;overload;
+    function AddArg(Const AName:String;Const AArg:Variant):IEZThread;overload;
     (*
       add several arguments at one time, note that this doesn't offer
       a cleanup method, so if an argument requires one, utilize the single
@@ -94,14 +135,22 @@ type
       sets up the thread with methods to run for starting/error/success
     *)
     function Setup(Const AStart:TThreadCallback;
-      Const AError:TThreadCallback=nil;Const ASuccess:TThreadCallback=nil):IEZThread;overload;
+      Const AError:TThreadCallback;Const ASuccess:TThreadCallback):IEZThread;overload;
+    function Setup(Const AStart:TThreadCallback):IEZThread;overload;
+
     function Setup(Const AStart:TThreadMethod;
-      Const AError:TThreadMethod=nil;Const ASuccess:TThreadMethod=nil):IEZThread;overload;
+      Const AError:TThreadMethod;Const ASuccess:TThreadMethod):IEZThread;overload;
+    function Setup(Const AStart:TThreadMethod):IEZThread;overload;
 
     (*
       starts the thread
     *)
     procedure Start;
+
+    (*
+      stops thread if started
+    *)
+    procedure Stop;
   end;
 
   { TEZArg }
--- test/ezthreads_tester.lpr
+++ test/ezthreads_tester.lpr
@@ -1,7 +1,47 @@
 program ezthreads_tester;
 
-uses ezthreads;
+uses
+  SysUtils,
+  ezthreads;
+
+var
+  Thread : IEZThread;
+  Waiting : Boolean;
+
+  procedure UpdateWaiting(Const AThread:IEZThread);
+  begin
+    Waiting:=False;
+  end;
+
+  procedure Start(Const AThread:IEZThread);
+  begin
+    //todo - sample showing fetching arguments and using them
+  end;
 
 begin
+  Waiting:=True;
+  //todo - init this to a new ezthread
+  Thread:=nil;
+
+  //below we show how we can setup an ezthread with a few arguments
+  //and an on stop callback
+  Thread
+    .Settings
+      //maximum runtime is 50msecs will terminate if takes longer
+      .UpdateMaxRuntime(50)
+      .Thread
+    .Events
+      .UpdateOnStopCallback(@UpdateWaiting)
+      .Thread
+    .AddArg('test_string','hello world')
+    .AddArg('test_int',5)
+    .Setup(@Start)
+    .Start;
+
+  //simple loop to see when our thread finishes
+  while Waiting do
+    Sleep(100);
+
+  WriteLn('waiting no longer');
 end.
 
