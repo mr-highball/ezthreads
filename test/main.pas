99d7291c2d9ac666627dcd60ff5846a676c5e1c0
mr-highball <35942565+mr-highball@users.noreply.github.com>
2018-09-08 23:57:46 -0400

    ui test
    -added a tester focuesed on ui applications
    -simple edit test in tabsheet format (new tests will have own tab)

---
+++ test/main.pas
@@ -0,0 +1,83 @@
+unit main;
+
+{$mode delphi}{$H+}
+{$ModeSwitch nestedprocvars}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
+  StdCtrls, ezthreads;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    btn_edit_doit: TButton;
+    edit_test: TEdit;
+    pctrl_main: TPageControl;
+    ts_edit: TTabSheet;
+    procedure btn_edit_doitClick(Sender: TObject);
+  private
+    procedure UpdateEditInMainThread(Const AThread:IEZThread);
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.btn_edit_doitClick(Sender: TObject);
+var
+  LThread:IEZThread;
+
+  procedure WaitSomeTime(Const AThread:IEZThread);
+  begin
+    //sleep 10 seconds before updating the text
+    Sleep(10000);
+  end;
+
+begin
+  if not btn_edit_doit.Enabled then
+    Exit;
+  //update the edit control with some different text
+  LThread:=TEZThreadImpl.Create;
+
+  //configure the thread and start it
+  LThread
+    .Settings
+      .UpdateSynchronizeStopEvents(True)
+      .Thread
+    .Events
+      .UpdateOnStop(UpdateEditInMainThread)
+      .Thread
+    .AddArg('text','a message from a thread')
+    .Setup(WaitSomeTime)
+    .Start;
+
+  //disable control so user cannot click it a lot
+  btn_edit_doit.Enabled:=False;
+end;
+
+procedure TMainForm.UpdateEditInMainThread(const AThread: IEZThread);
+var
+  LMsg:String;
+begin
+  if ThreadID<>MainThreadID then
+    raise Exception.Create('needs to be in main thread');
+
+  //set this for debugging, but optional variable, could just assign from thread
+  LMsg:=AThread['text'];
+  edit_test.Text:=LMsg;
+  btn_edit_doit.Enabled:=True;
+end;
+
+end.
+
